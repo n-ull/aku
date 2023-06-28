3aaddba3354f15915afc76389811cd2ef321cd0b
n-ull <felix.greed@gmail.com>
2023-06-27 23:52:48 -0300

    junk deleted

--- utils/to_thread.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import typing
-import functools
-
-def to_thread(func: typing.Callable) -> typing.Coroutine:
-    @functools.wraps(func)
-    async def wrapper(*args, **kwargs):
-        return await asyncio.to_thread(func, *args, **kwargs)
-    return wrapper
